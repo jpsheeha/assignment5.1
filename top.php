699cbaacb564539d05d3c489356bfbedcd7534cd
jpsheeha <jpsheeha@uvm.edu>
2013-11-07 17:05:03 -0500

    Added a new font
    Font will be used for titles

--- top.php
+++ top.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="author" content="Jason Sheehan">
 <meta name="description" content="Pokemon Trainer">
+<link href='http://fonts.googleapis.com/css?family=Sniglet:400,800' rel='stylesheet' type='text/css'>
 
 <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
